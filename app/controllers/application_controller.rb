f1591b82e692f1b01b55c5b84f1b62733dd8945f
Josh McMillan <josh@joshmcmillan.co.uk>
2020-11-27 01:58:42 +0000

    Disallow access to non-admins in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_devise_parameter_sanitizer, if: :devise_controller?
+  before_action :reject_non_admins
 
   private
 
   def configure_devise_parameter_sanitizer
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name address])
+  end
+
+  def reject_non_admins
+    return unless Rails.env.production?
+    return if user_signed_in? && current_user.admin?
+    return if controller_path == 'devise/sessions'
+
+    head :forbidden
   end
 end
