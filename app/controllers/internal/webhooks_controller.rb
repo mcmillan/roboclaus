e55417f6c6679bb8e1c666dfa621748cd7d5a1b6
Josh McMillan <josh@joshmcmillan.co.uk>
2020-12-05 02:14:09 +0000

    Remove blocked phone numbers with "start" command

--- app/controllers/internal/webhooks_controller.rb
+++ app/controllers/internal/webhooks_controller.rb
@@ -1,16 +1,26 @@
 class Internal::WebhooksController < ActionController::API
   def twilio
-    return render xml: Twilio::TwiML::MessagingResponse.new if BlockedPhoneNumber.exists?(phone_number: params[:To])
+    blocked = BlockedPhoneNumber.exists?(phone_number: params[:To])
 
-    if params[:Body].to_s.strip.downcase == 'stop'
-      BlockedPhoneNumber.create(phone_number: params[:To])
+    if blocked && params[:Body].to_s.strip.downcase == 'start'
+      BlockedPhoneNumber.where(phone_number: params[:To]).destroy_all
+
+      return render xml: Twilio::TwiML::MessagingResponse.new do |r|
+        r.message body: '🎅 Welcome back to Roboclaus!'
+      end
+    elsif blocked
       return render xml: Twilio::TwiML::MessagingResponse.new
     end
 
-    twiml = Twilio::TwiML::MessagingResponse.new do |r|
-      r.message body: 'No longer feeling the whole Christmas thing? Reply STOP to this message.'
+    if params[:Body].to_s.strip.downcase == 'stop'
+      BlockedPhoneNumber.create(phone_number: params[:To])
+      return render xml: Twilio::TwiML::MessagingResponse.new do |r|
+        r.message body: "You'll no longer be sent any messages by Roboclaus."
+      end
     end
 
-    render xml: twiml
+    render xml: Twilio::TwiML::MessagingResponse.new do |r|
+      r.message body: 'No longer feeling the whole Christmas thing? Reply STOP to this message.'
+    end
   end
 end
