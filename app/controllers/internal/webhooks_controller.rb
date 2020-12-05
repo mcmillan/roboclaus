e907c4583f77b560d0133c2e4de36384da2a7ddb
Josh McMillan <josh@joshmcmillan.co.uk>
2020-12-05 02:45:03 +0000

    DRY up references to message body

--- app/controllers/internal/webhooks_controller.rb
+++ app/controllers/internal/webhooks_controller.rb
@@ -1,8 +1,9 @@
 class Internal::WebhooksController < ActionController::API
   def twilio
     blocked = BlockedPhoneNumber.exists?(phone_number: params[:From])
+    body = params[:Body].to_s.strip.downcase
 
-    if blocked && params[:Body].to_s.strip.downcase == 'start'
+    if blocked && body == 'start'
       BlockedPhoneNumber.where(phone_number: params[:From]).destroy_all
 
       twiml = Twilio::TwiML::MessagingResponse.new do |r|
@@ -16,13 +17,17 @@ class Internal::WebhooksController < ActionController::API
       end
 
       return render xml: twiml
-    end
-
-    if params[:Body].to_s.strip.downcase == 'stop'
+    elsif body == 'stop'
       BlockedPhoneNumber.create(phone_number: params[:From])
 
       twiml = Twilio::TwiML::MessagingResponse.new do |r|
         r.message body: "❌ You'll no longer be sent any messages by Roboclaus."
+      end
+
+      return render xml: twiml
+    elsif %w[69 420].include?(body)
+      twiml = Twilio::TwiML::MessagingResponse.new do |r|
+        r.message body: '🤙 nice'
       end
 
       return render xml: twiml
