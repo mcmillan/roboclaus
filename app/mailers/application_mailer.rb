91a56a0869eacc211a84471feec9a30e0bcb638a
Josh McMillan <josh@joshmcmillan.com>
2022-11-08 21:17:01 +0000

    Send email from hohoho@robocla.us

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Roboclaus <no-reply@robocla.us>', reply_to: 'hohoho@robocla.us'
+  default from: 'Roboclaus <hohoho@robocla.us>'
   layout 'mailer'
 end
